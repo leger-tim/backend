8a49beb6e5bc6f5dab7ef9e86266a16ad64a2cf6
Tim <leger.esperandieu.t@gmail.com>
2023-02-16 14:53:45 +0100

    Routes et controllers sauce

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
 const path = require('path');
 const app = express();
 
@@ -24,6 +25,7 @@ app.use((req, res, next) => {
 // });
 
 app.use('/api/auth', userRoutes);
+app.use('/api/sauces', sauceRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
